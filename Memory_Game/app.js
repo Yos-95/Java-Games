b4ac680d4fff2be04bd3f360ed70cf62c588eb47
Yogesh Kumar <yogesh.sahu8@gmail.com>
2023-04-13 20:20:38 +0530

    finished basic game need work on css and js

--- Memory_Game/app.js
+++ Memory_Game/app.js
@@ -52,14 +52,65 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 function createBoard() {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
     }
 }
 
 createBoard()
+
+function checkMatch() {
+    const cards = document.querySelectorAll('#grid img')
+
+    console.log('Check for Match')
+    if (cardsChosenIds[0] == cardsChosenIds[1]) {
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/blank.png')
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/blank.png')
+        alert('You have clicked the same image')
+    }
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('You found a match')
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png')
+        cards[cardsChosenIds[0]].removeEventListener('click', flipCard)
+        cards[cardsChosenIds[1]].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/blank.png')
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/blank.png')
+        alert('Sorry Try Again')
+    }
+    resultDisplay.textContent = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if (cardsWon.length == cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulations you found them all'
+
+    }
+}
+
+function flipCard() {
+    // console.log(cardArray)
+    const cardId = this.getAttribute('data-id')
+    // console.log(cardArray[cardId].name)
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    // console.log('clicked', cardId)
+    // console.log(cardsChosen)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2){
+        setTimeout(checkMatch, 500)
+    }
+
+}
